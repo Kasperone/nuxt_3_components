6a8324b182d30877a4a81b87e85c6d04b6829d0b
kasperone <kasper.kosecki@antologic.com>
2024-09-15 23:51:17 +0200

    feat(NC-001): installed nuxt-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
   typescript: {
     typeCheck: true,
   },
+
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ["@nuxt/ui"],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Index page</h1>
+  <div>
+    <h1>Index Page</h1>
+    <UIcon name="i-heroicons-light-bulb" :size="24" class="text-purple-500" />
+  </div>
 </template>
+
+<script setup lang="ts"></script>
